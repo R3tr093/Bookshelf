4fd2a72413bb7859e8806cc7180108b5d8252a2b
Pirghaye Fabian <pirghaye.f@gmail.com>
2019-10-15 10:20:38 +0200

    Revert "Merge branch 'develop'"
    This reverts commit 40965b932f7f0f6f6a6697e2883715a6f8a4d6ab.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Apollo } from 'apollo-angular';
-import gql from 'graphql-tag';
-import { QueriesServices } from './services/queries.services';
-import { AuthServices} from './services/auth.services';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  
+export class AppComponent {
   title = 'Bookshelf';
-  
-  books: any;
-
-  constructor(apollo: Apollo, private QueriesService: QueriesServices, private AuthService: AuthServices) {}
-
-  ngOnInit(){}
-
-
-
-  
-
-
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,96 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpHeaders } from '@angular/common/http';
-import { HttpClientModule } from '@angular/common/http';
-import { ApolloModule, Apollo } from 'apollo-angular';
-
-import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloLink } from 'apollo-link';
-
-import { QueriesServices } from './services/queries.services';
-import { AuthServices} from './services/auth.services';
-import { ErrorComponent } from './error/error.component';
-
-import { LibraryComponent } from './library/library.component';
-import { AuthComponent } from './auth/auth.component';
-import { ArticleComponent } from './article/article.component';
-import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-
-registerLocaleData(en);
-
-
-
-
-      
-
-const appRoutes: Routes = 
-[
-    { path: '',component: AuthComponent },
-    { path: 'library', component: LibraryComponent},
-    { path: 'article/:isbn', component: ArticleComponent},
-    { path: '**', component: ErrorComponent }
-];
-
 @NgModule({
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    ApolloModule,
-    HttpLinkModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    ),
-    NgZorroAntdModule,
-    BrowserAnimationsModule
-
+    AppRoutingModule
   ],
-  declarations: [AppComponent, ErrorComponent, LibraryComponent, AuthComponent, ArticleComponent],
-  bootstrap: [AppComponent],
-  providers: [
-  
-    QueriesServices,
-    AuthServices,
-    { provide: NZ_I18N, useValue: en_US }
-
-  ]
+  providers: [],
+  bootstrap: [AppComponent]
 })
-
-// Setting token and apollo. 
-export class AppModule {
-  constructor(apollo: Apollo, httpLink: HttpLink, private AuthService: AuthServices) {
-
-    const http = httpLink.create({uri: 'https://graph.becode.xyz/'});
-
-    const authLink = new ApolloLink((operation, forward) => {
- 
-
-      operation.setContext({
-        headers: {
-          // Remove this by const token ? 
-          Authorization: 'Bearer ' + this.AuthService.usrToken        
-
-        }
-      });
-
-      return forward(operation);
-
-    });
-
-    apollo.create({
-      link: authLink.concat(http),
-      cache: new InMemoryCache()
-    });
-
-  }
-}
+export class AppModule { }
